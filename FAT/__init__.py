9e7f8ec283470b0d91e90c498e8f42d6ff927775
Kamil Tokarski <kamil@tokarski.xyz>
2021-01-08 12:18:12 +0100

    Logging using app insight

--- FAT/__init__.py
+++ FAT/__init__.py
@@ -1,13 +1,30 @@
+import logging
+
 from flask import Flask
 from FAT.config import FATConfig
 from FAT.database import db, migrate
 
 from flask_sessions import Session
 
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace.samplers import ProbabilitySampler
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(FATConfig)
+
+    # App Insight logging
+    FlaskMiddleware(
+        app,
+        exporter=AzureExporter(connection_string=f'InstrumentationKey={app.config["APP_INSIGHT_KEY"]}'),
+        sampler=ProbabilitySampler(rate=1.0)
+    )
+    logger = logging.getLogger(__name__)
+    handler = AzureLogHandler(connection_string=f'InstrumentationKey={app.config["APP_INSIGHT_KEY"]}')
+    logger.addHandler(handler)
 
     # Sessions for AD auth
     Session(app)
