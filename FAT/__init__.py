da9a5758fe1f3be241db83e4f27ab8659f85536e
Kamil Tokarski <kamil@tokarski.xyz>
2021-01-08 12:12:10 +0100

    Add database support

--- FAT/__init__.py
+++ FAT/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from FAT.config import FATConfig
+from FAT.database import db, migrate
 
 from flask_sessions import Session
 
@@ -12,6 +13,10 @@ def create_app():
     Session(app)
     from werkzeug.middleware.proxy_fix import ProxyFix
     app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+    # Database
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     with app.app_context():
         import FAT.auth
--- FAT/config.py
+++ FAT/config.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 
 
 class FATConfig:
@@ -10,3 +11,21 @@ class FATConfig:
     AD_SECRET = os.getenv('AD_SECRET')
     AD_AUTHORITY = os.getenv('AD_AUTHORITY')
     AD_REDIRECT_PATH = "/auth/get_token"
+
+    # Azure SQL
+    DB_SERVER = os.getenv('DB_SERVER')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USERNAME = os.getenv('DB_USERNAME')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc:///?odbc_connect=' + urllib.parse.quote_plus(f'DRIVER={{SQL Server}};SERVER={DB_SERVER};DATABASE={DB_NAME};UID={DB_USERNAME};PWD={DB_PASSWORD}')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+    # Azure Blobs
+    BLOB_ACCOUNT = os.getenv('BLOB_ACCOUNT')
+    BLOB_KEY = os.getenv('BLOB_KEY')
+    BLOB_NAME = os.getenv('BLOB_NAME')
+
+    # Azure App Insight
+    APP_INSIGHT_KEY = os.getenv('APP_INSIGHT_KEY')
